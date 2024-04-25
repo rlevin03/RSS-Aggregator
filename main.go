f133444f75a70ffa8241951369f0241400c5f87c
Robert Levin <blevin0521@gmail.com>
2024-04-25 17:27:54 -0400

    Day 3

--- main.go
+++ main.go
@@ -29,6 +29,12 @@ func main() {
 		MaxAge:           300,
 	}))
 
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
